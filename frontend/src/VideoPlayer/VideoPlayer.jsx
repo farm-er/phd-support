674440097a829cf5cbcfbb8870ae6261a01a631a
apollo <majdoulioussama1@gmail.com>
2024-11-23 00:27:08 +0100

    added a video player but i need to hook the videos table and backend to the front end and handle youtube videos in topics

--- frontend/src/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './VideoPlayer.css'
 
 
 
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 
@@ -11,13 +11,69 @@ const VideoPlayer = () => {
 
     const [editorState, setEditorState] = useState(
         () => EditorState.createEmpty(),
-      );
+    );
+
+
+    const customStyleMap = {
+        HIGHLIGHT: { backgroundColor: 'red'},
+        BOLD: { fontWeight: 900},
+        FONT_SIZE_SMALL: { fontSize: '12px'},
+        FONT_SIZE_MEDIUM: { fontSize: '16px'},
+        FONT_SIZE_BIG: { fontSize: '24px'},
+    }
+
+
+    function handleBold(e) {
+        e.preventDefault()
+        e.target.classList.toggle('clicked')
+        setEditorState( RichUtils.toggleInlineStyle( editorState, 'BOLD'))
+    
+    }
+
+    function handleHighlight(e) {
+        e.preventDefault()
+        e.target.classList.toggle('clicked')
+        setEditorState( RichUtils.toggleInlineStyle( editorState, 'HIGHLIGHT'))
+    }
+
+    useEffect( () => {
+        console.log(editorState.getCurrentInlineStyle())
+    }, [editorState])
+
+    function handleFontSize(e) {
+
+        const fontSize = e.target.value
+
+
+        if (fontSize) setEditorState( RichUtils.toggleInlineStyle( editorState, fontSize))
+    }
 
 
     return (
         <div className="VideoPlayer">
             <div className="VideoNote">
-                <Editor editorState={editorState} onChange={setEditorState} />
+                <div className="VideoEditorWrapper">
+                    <Editor
+                        editorState={editorState}
+                        customStyleMap={customStyleMap}
+                        onChange={(editorState) => { setEditorState(editorState)}}
+                    />
+                </div>
+                <div className="VideoNoteToolbar">
+                    <select
+                        onChange={ (e) => handleFontSize(e)}
+                    >
+                        <option value="FONT_SIZE_SMALL">S</option>
+                        <option value="FONT_SIZE_MEDIUM">M</option>
+                        <option value="FONT_SIZE_BIG">L</option>
+                    </select>
+                    <button
+                        onMouseDown={ (e) => handleBold(e)}
+                    >G</button>
+                    <button
+                        onMouseDown={ (e) => handleHighlight(e)}
+                    >H</button>
+                </div>
             </div>
             <div className="VideoSection">
                 <iframe
@@ -27,6 +83,10 @@ const VideoPlayer = () => {
                     referrerPolicy="strict-origin-when-cross-origin"
                     allowFullScreen
                 ></iframe>
+
+                <div className="VideoTitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                </div>
             </div>
         </div>
     );
